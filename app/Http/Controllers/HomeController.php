cb481a5c1ed57378651e33656e858d95565c1566
meiwenhui <meiwenhui@163.com>
2024-12-12 17:12:20 +0800

    格式化

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Admin\Archive\UpdateArchiveRequest;
 use App\Models\ArchiveModel;
-use App\Models\WordModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 
 class HomeController extends Controller
 {
 
     public function index(Request $request)
     {
-        $archives = ArchiveModel::query()->limit(1)->get();
-
+        $archives = ArchiveModel::query()->orderBy('word')->get();
         $data = ['archives' => $archives];
-
         return view('welcome', $data);
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 </head>
 
 <body class="">
-
     <main class="container" style="margin-top: 10rem;">
-
         @foreach ($archives as $archive)
-            <p>这是用户 {{ $archive->title }}</p>
+            <h4>{{ $archive->title }}</h4>
+            <p>{{ $archive->content }}</p>
+            <hr/>
         @endforeach
-
-
-
-
-
-
-
-
-
-
-
     </main>
-
-
-
-    <script>
-        const toastTrigger = document.getElementById('gradeSelect')
-        if (toastTrigger) {
-            toastTrigger.addEventListener('change', (e) => {
-                console.log(e)
-            })
-        }
-    </script>
-
 </body>
 
 </html>
